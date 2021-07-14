f7dde3042ae1aa3ea6787b23cb84bcb8e4937253
efollana-sistel <40859287+efollana-sistel@users.noreply.github.com>
2021-07-14 11:36:57 +0200

    Fix for Linux environment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App';
-import './index.scss';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+// Prime React CSS
+import '../node_modules/primereact/resources/themes/saga-blue/theme.css';
+import '../node_modules/primereact/resources/primereact.min.css';
+import '../node_modules/primeicons/primeicons.css';
 
 ReactDOM.render(
     <BrowserRouter>
